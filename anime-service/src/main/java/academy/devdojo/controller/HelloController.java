d186ac3ad1dcefd9ad38376c23597f0baa1c417d
William Suane <william.suane@devdojo.academy>
2024-06-27 14:42:42 +0200

    video08: Spring Beans and ComponentScan

--- anime-service/src/main/java/academy/devdojo/controller/HelloController.java
+++ anime-service/src/main/java/academy/devdojo/controller/HelloController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 
     @GetMapping("hi")
-    public String hi(){
+    public String hi() {
         return "OMAE WA MOU SHINDE IRU";
     }
 }
